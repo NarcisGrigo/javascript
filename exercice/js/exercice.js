1cdcf4d1eadaf7ab721438f0bfd83cfe6f03133f
NarcisGrigo <132442585+NarcisGrigo@users.noreply.github.com>
2023-05-24 14:22:46 +0200

    Initial commit

---
+++ exercice/js/exercice.js
@@ -0,0 +1,70 @@
+// var Prenom = "Narcis";   //  string  \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// var monNumero = 69;   //  numérique / number  \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// var monNum = "420";   //  string  \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+//  var nom = Kira;   //  faux (n'existe pas)  \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// var booleanFalse = false;  //  boolean  \\
+// var booleanTrue = true;  //  boolean  \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// var varNull = null;  //  null  \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// console.log (Prenom);  // permet l'affichage d'une variable dans la console \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// alert (Prenom);
+// alert ("Mon prénom est : Narcis");
+// window.alert ("Super, tu es arrivé sur mon site !");   // --- window.alert ou alert pour afficher une boite d'alerte --- \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// document.write ("A la pause vous aurez des Haribos Pik !");   // --- document.write pour afficher le texte dans la page web --- \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// prompt ("Question: on est quel jour ?", "jour de la semaine");   // --- window.prompt ou prompt pour afficher une boite de dialogue ou on peut écrire et afficher nos données --- \\
+//var jour = prompt ();   // --- affiche un formulaire , vide parce-que on a pas donné une valeur --- \\
+//document.write (jour);   // --- affiche le texte stockée dans la variable donnée sur la page web --- \\
+                                                                   //////////////////////////////////////////////////////////////////////////
+// var unBooleen = false;
+// console.log (unBooleen);
+// unBooleen = "true";   // --- la derniere surcharge la premiere --- \\
+// console.log (unBooleen);
+// console.log (typeof unBooleen);
+                                                                   //////////////////////////////////////////////////////////////////////////
+// const PAYS = "France";
+// PAYS = "Italie"   // --- On déclare uneseule fois --- \\
+// document.write (PAYS);
+                                                                   //////////////////////////////////////////////////////////////////////////
+// var unChiffre = "69.420724";
+// console.log (unChiffre);
+// console.log (typeof unChiffre);
+                                                                    /////////////////////////////////////////////////////////////////////////
+// STRING => NUMBER
+// monChiffre = parseInt (unChiffre);   // --- coupe tout ce qui est après la virgule du coup 69.420=69 --- \\  // --- transforme un string en nombre/number ---\\
+// console.log (monChiffre);
+// console.log (typeof monChiffre);
+                                                                   //////////////////////////////////////////////////////////////////////////
+// STRING => FLOAT
+// unChiffre = parseFloat (unChiffre);   // --- transforme un nombre/number entier/integer en nombre decimal --- \\
+// console.log (unChiffre);
+// console.log (typeof unChiffre);
+                                                                   //////////////////////////////////////////////////////////////////////////
+// NUMBER => STRING
+// var nb_en_lettres = 258;
+// console.log (nb_en_lettres);
+// console.log (typeof nb_en_lettres);
+                                                                    //////////////////////////////////////////////////////////////////////////
+// var nb_en_lettres = nb_en_lettres.toString ();   // --- transforme un nombre en string et affecte le résultat sur la variable elle meme --- \\
+// console.log (nb_en_lettres);
+// console.log (typeof nb_en_lettres);
+
+                                                                                                                    // --- FONCTIONS --- \\
+
+
+   // --- parenthèses = fonction --- \\  // --- ce qui est dans les parenthèses sont des paramètres --- \\
+
+
+                                                                                                                       // --- EXERCICES --- \\
+
+var prenom = prompt ("Narcis");
+alert (prenom)
+console.log (prenom);
+document.write (prenom)
