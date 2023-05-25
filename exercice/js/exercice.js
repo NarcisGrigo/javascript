cda3dfff07f8ba435265397ea346f03eb998f2c3
NarcisGrigo <132442585+NarcisGrigo@users.noreply.github.com>
2023-05-25 12:29:10 +0200

    updated exercices

--- exercice/js/exercice.js
+++ exercice/js/exercice.js
@@ -76,13 +76,32 @@
 
                                                                     //////////////////////////////////////////////////////////////////////////
 
+// 1.	Affichage d’une boite de dialogue avec une case de formulaire vide
+// 2.	Déclarer une variable nb1
+// 3.	Affecter le contenu du formulaire dans cette variable
+// 4.	Saisir un entier dans le formulaire (sachant que quel que soit la valeur saisie dans le formulaire est de type string)
+// 5.	Déclarer une variable qui s’appelle nb2 et affecter le chiffre 7 à cette variable
+// 6.	Calculer le modulo de nb1 par nb2 et affecter le résultat dans la variable nommée (‘nb3’ ou ‘resultat’)
+// 7.	Affichage du résultat sur la page web
+// 8.	Affichage du résultat sur la console
+// 9.	Affichage du résultat à partir d’une boîte de dialogue
+// 10.	Additionner (‘nb3’ ou ‘resultat’) avec nb2 et stocker le dans (‘nb3’ ou ‘resultat’)
+// 11.
+
+                                                                   //////////////////////////////////////////////////////////////////////////
+
 var nb1 = prompt  ("Sasir un entier dans le formulaire");
 console.log(nb1);
+nb1 = parseInt(nb1);
+console.log(typeof nb1);
 var nb2 = 7;
 console.log(nb2);
+console.log(typeof nb2);
 var resultat = nb1 % nb2;
-resultat = resultat.toString ();
-console.log(resultat);
-console.log(typeof resultat);
+// resultat = resultat.toString ();
+// console.log(resultat);
+// console.log(typeof resultat);
 alert(resultat);
+document.write(resultat + " ");
+resultat += nb2;
 document.write(resultat);
