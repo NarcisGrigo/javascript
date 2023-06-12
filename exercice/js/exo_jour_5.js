b0e94b01f3cc6b2d7d4cdc67c78845c8bf43b5ca
NarcisGrigo <132442585+NarcisGrigo@users.noreply.github.com>
2023-06-12 16:53:33 +0200

    javascript

---
+++ exercice/js/exo_jour_5.js
@@ -0,0 +1,29 @@
+// var age = prompt("Indiquez votre age : ");
+
+// while (age <= 3 || age >= 120 || isNaN(age)) {
+//     age = prompt("Indiquez votre age : ");
+// }
+
+// var majoriteFR = 18;
+
+// function afficheAge(agePersonne, ageAdulte) {
+//     if (agePersonne < ageAdulte) {
+//         alert("Vous n'etes pas encore majeur ! ");
+//     }
+
+//     if (agePersonne >= ageAdulte) {
+//         alert("Bienvenue, vous etes majeur ! ");
+//     }
+// }
+
+// afficheAge(age, majoriteFR);
+
+// function afficheAge(agePersonne, ageAdulte) {
+//   if (agePersonne < ageAdulte) {
+//     alert("Vous n'êtes pas encore majeur ! ");
+//   }
+
+//   if (agePersonne >= ageAdulte) {
+//     alert("Bienvenue, vous êtes majeur. :)");
+//   }
+// }
