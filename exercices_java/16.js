e510617ee86372fe7d4e600ebcf3dc668a126077
NarcisGrigo <132442585+NarcisGrigo@users.noreply.github.com>
2023-06-22 12:25:01 +0200

    Update 16.js

--- exercices_java/16.js
+++ exercices_java/16.js
@@ -1,9 +1,25 @@
 // alert('test');
 
+//    && = et
+//    || = ou
+
 /* --- L'opérateur d'inégalité stricte ( !== ) vérifie si ses deux opérandes ne sont pas égaux et renvoie un booléen correspondant au résultat --- */
 
 var disMoi = "";
+alert("dismoi A: " + disMoi);
 var i = 1;
+alert("i A: " + i);
+
+// (disMoi !== "oui" || disMoi !== "non") &&
+//   (disMoi !== "Oui" || disMoi !== "Non") &&
+//   (disMoi !== "OUI" || disMoi !== "NON");
+
+// (disMoi !== "oui" ||
+//   disMoi !== "non" ||
+//   disMoi !== "Oui" ||
+//   disMoi !== "Non" ||
+//   disMoi !== "OUI" ||
+//   disMoi !== "NON");
 
 while (
   disMoi !== "oui" &&
@@ -14,6 +30,8 @@ while (
   disMoi !== "NON"
 ) {
   disMoi = prompt("Allez!! On joue à ni oui ni non?");
+
+  alert("dismoi B: " + disMoi);
   document.write(
     '"' +
       disMoi +
@@ -21,9 +39,17 @@ while (
       i +
       "° fois, veux-tu jouer oui ou non? <br>"
   );
+
+  alert("i B: " + i);
   i++;
+
+  alert("i C: " + i);
 }
-i--; //parce qu'il y a une incrémentation en trop
+
+alert("i D: " + i);
+i--;
+
+alert("i E: " + i); //parce qu'il y a une incrémentation en trop
 document.write(
   "<p style='background-color:cyan; text-align:center;'>PERDU ;-) ! Tu as tenu " +
     i +
