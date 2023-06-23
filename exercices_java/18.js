6d689ad5bbb7e0b6c236055a47b1d24d44b7f6ea
NarcisGrigo <132442585+NarcisGrigo@users.noreply.github.com>
2023-06-23 10:14:51 +0200

    javascript

--- exercices_java/18.js
+++ exercices_java/18.js
@@ -1,7 +1,13 @@
-// Solution utilisant une série de if/else
-for (var nombre = 1; nombre <= 100; nombre++) {
-    if ((nombre % 3 === 0) && (nombre % 5 === 0)) console.log("FizzBuzz");
-    else if (nombre % 3 === 0) console.log("Fizz");
-    else if (nombre % 5 === 0) console.log("Buzz");
-    else console.log(nombre);
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    document.write("FizzBuzz <br>");
+  } else if (i % 5 == 0) {
+    document.write("Buzz <br>");
+  } else if (i % 3 == 0) {
+    document.write("Fizz <br>");
+  } else {
+    document.write(i + "<br>");
+  }
 }
+/*  for = boucle  */
+/*  if / else if / else = conditions   --- faut avoir des acolades pour les conditions  */
